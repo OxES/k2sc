f93a7bdf9b456487fab7af93eaaccf66d8b935aa
Hannu Parviainen <hpparvi@gmail.com>
2015-11-18 14:11:30 +0000

    Changed to use SAP instead of PDCSAP

--- src/k2io.py
+++ src/k2io.py
@@ -84,8 +84,8 @@ class MASTReader(DataReader):
                       time=data['time'],
                       cadence=data['cadenceno'],
                       quality=data['sap_quality'],
-                      fluxes=data['pdcsap_flux'],
-                      errors=data['pdcsap_flux_err'],
+                      fluxes=data['sap_flux'],
+                      errors=data['sap_flux_err'],
                       x=data['pos_corr1'],
                       y=data['pos_corr2'],
                       sap_header=head)    
@@ -97,7 +97,7 @@ class MASTReader(DataReader):
             return False
         else:
             h = pf.getheader(fname, 1)
-            fmt_ok = 'PDCSAP_FLUX' in h.values()
+            fmt_ok = 'SAP_FLUX' in h.values()
             return fmt_ok
         
 
