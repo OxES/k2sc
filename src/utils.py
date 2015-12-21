e4c8e842cb66b3ff408399b89ac9c1d0d9aed066
Hannu Parviainen <hpparvi@gmail.com>
2015-12-21 11:46:25 +0000

    Added a simple fold function.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
 from numpy import isfinite, nan, median, abs
 
+def fold(time, period, origo=0.0, shift=0.0, normalize=True):
+    """Folds the given data over a given period.
+    """
+    tf = ((time - origo)/period + shift) % 1.
+    return tf if normalize else tf*period
+
+
 def medsig(a):
     """Return median and outlier-robust estimate of standard deviation
        (1.48 x median of absolute deviations).
