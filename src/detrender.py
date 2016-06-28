8b59cfc6cde16ce08f825ed5f75042d57e37d428
Hannu Parviainen <hpparvi@gmail.com>
2016-06-28 12:09:28 +0100

    Separated Detrender and DtData and cleaned up DtData.

--- src/detrender.py
+++ src/detrender.py
@@ -1,15 +1,10 @@
 from __future__ import division
 
-from scipy.optimize import fmin, fmin_powell, minimize
-from george import GP
-from george.kernels import ExpSquaredKernel as ESK
-from george.kernels import ExpSine2Kernel as ESn2K
-
 import math as m
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as pl
 
+from scipy.optimize import fmin, fmin_powell, minimize
 from numpy import (any, array, asarray, ones, ones_like, zeros, isfinite, inf, concatenate, arange, unique, delete,
                    dot, median, abs, std, nan, diag, log, where, identity, s_, sqrt)
 from numpy.random import permutation
@@ -18,18 +13,19 @@ from matplotlib.pyplot import subplots, setp
 from gp import GeorgeGP, SplitGP
 from kernels import BasicKernel
 from utils import medsig
-
-c_ob = '#002147'
-c_bo = '#BF5700'
+from dtdata import DtData
 
 class Detrender(object):
-    def __init__(self, flux, inputs, mask=None, p0=None, kernel=None, splits=[], tr_nrandom=100, tr_bspan=50, tr_nblocks=5):
+    def __init__(self, flux, inputs, mask=None, p0=None, kernel=None, splits=[], tr_nrandom=200, tr_bspan=50, tr_nblocks=6):
         self.data   = DtData(flux, inputs, mask)
         self.kernel = kernel or BasicKernel()
         self.gp     = SplitGP(self.kernel, splits) if splits is not None else GeorgeGP(self.kernel)
-        #self.mask_outliers(max_sigma=5, pv=self.kernel._pv)
         self.tr_data  = self.data.create_training_set(tr_nrandom, tr_bspan, tr_nblocks)
         self.gp.set_inputs(self.tr_data.masked_inputs)
+
+    ## ======================
+    ##  Convenience routines
+    ## ======================
 
     @property
     def flux(self):
@@ -39,22 +35,10 @@ class Detrender(object):
     def time(self):
         return self.data.masked_time
 
-    @property
-    def outliers(self):
-        return self.data.outliers
 
-
-    def mask_outliers(self, max_sigma=5, pv=None):
-        pv = self.kernel.pv0 if pv is None else pv
-        qm = self.data.quality_mask
-        predicted_flux = self.predict(pv)
-        residuals = self.data.unmasked_normalised_flux - predicted_flux
-        flux_median, flux_std = medsig(residuals[qm])
-        outlier_mask = ones_like(self.data.mask)
-        outlier_mask[qm] = abs(residuals[qm]-flux_median) < (max_sigma * flux_std)
-        self.data.set_mask(outlier_mask)
-        return outlier_mask.sum()
-
+    ## =====================
+    ##  Detrending routines
+    ## =====================
 
     def covariance_matrix(self, pv=None, inputs=None, separate=False):
         inputs = inputs if inputs is not None else self.tr_data.masked_inputs
@@ -97,45 +81,52 @@ class Detrender(object):
         flux[~self.data.mask] = nan
         return flux
     
-    def plot_xy(self, pv=None, ax=None):
+
+    ## ===================
+    ##  Plotting routines
+    ## ===================
+
+    def plot_xy(self, pv=None, ax=None, plot_wireframe=False):
+        """Plot the x and y points for the whole dataset and the training set.
+        """
         if ax is None:
             fig,ax = subplots(1,1, figsize=(10,10))
         
         if pv is None:
-            ax.tripcolor(self.data.masked_inputs[:,1], self.masked_data.inputs[:,2], ones(self.npt), vmin=0, vmax=1)
-            ax.triplot(self.data.masked_inputs[:,1], self.data.masked_inputs[:,2], color='w')
+            ax.tripcolor(self.data.mx, self.data.my, ones(self.data.nptm), vmin=0, vmax=1)
+            if plot_wireframe:
+                ax.triplot(self.data.mx, self.data.my, color='w')
         else:
             mt, mp = self.compute_components(pv)
-            ax.tripcolor(self.data.masked_inputs[:,1], self.data.masked_inputs[:,2], mp)
+            ax.tripcolor(self.data.mx, self.data.my, mp)
             
-        ax.plot(self.tr_data.masked_inputs[:,1], self.tr_data.masked_inputs[:,2],'.', ms=8, c='w', alpha=0.8)
-        ax.plot(self.tr_data.masked_inputs[:,1], self.tr_data.masked_inputs[:,2],'.', ms=5, c='k')
+        ax.plot(self.tr_data.mx, self.tr_data.my, 'o', ms=3, c='k', mec='w')
         return ax
     
     
     def plot_t(self, pv=None, ax=None):
+        """ Plot the flux as a function of time for the whole dataset and the training set.
+        """
         if ax is None:        
-            fig,ax = subplots(1,1)
+            fig, ax = subplots(1,1)
             
-        fm = median(self.data.masked_flux)
+        fm = self.data.flux_median
         fmin = self.data.masked_flux.min()
         fmax = self.data.masked_flux.max()
         fptp = self.data.masked_flux.ptp()
 
-        ax.plot(self.data.masked_time, self.data.masked_flux, c='0.75', lw=1)
-        ax.plot(self.tr_data.unmasked_time, self.tr_data.unmasked_flux, '.k', ms=6)
-        ax.plot(*self.data.outliers, c='#BF5700', ls='', marker='o', ms=6)
-
+        ax.plot(self.data.mt, self.data.mf, c='0.75', lw=1)
+        ax.plot(self.tr_data.ut, self.tr_data.uf, '.k', ms=6)
         setp(ax, ylim=(0.999*fmin,1.001*fmax))
 
         if pv is not None:
             fd  = self.detrend_spatial(pv)
             fd +=  fm - np.nanmedian(fd)
             mm = isfinite(fd)
-            ax.plot(self.data.unmasked_time[mm], fd[mm] - 0.7*fptp, alpha=0.75, lw=1, c=c_ob)
+            ax.plot(self.data.unmasked_time[mm], fd[mm] - 0.7*fptp, alpha=0.75, lw=1)
             setp(ax, ylim=(0.999*(fmin-0.7*fptp), 1.001*fmax))
 
-        setp(ax, xlim=self.data.inputs[[0,-1],0], xlabel='Time', ylabel='Flux')
+        setp(ax, xlim=self.data.mt[[0,-1]], xlabel='Time', ylabel='Flux')
         return ax
 
     
@@ -149,18 +140,16 @@ class Detrender(object):
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
         ax.set_zorder(-1000)
-        ax.add_patch(pl.Rectangle((0,0.92), 1, 0.08, fill=True, color=c_ob))
+        ax.add_patch(pl.Rectangle((0,0.92), 1, 0.08, fill=True))
 
         ax_a = fig.add_axes([lmargin,0.25,1-lmargin-rmargin,0.3])
         ax_x = fig.add_axes([lmargin,0.05,1-lmargin-rmargin,0.1])
         ax_y = fig.add_axes([lmargin,0.15,1-lmargin-rmargin,0.1])
         ax_c = fig.add_axes([0.55,0.6,0.45-rmargin, 0.3])
-
  
-        ax_x.plot(self.data.masked_time, self.data.masked_inputs[:,1], lw=1, c=c_ob)
-        ax_y.plot(self.data.masked_time, self.data.masked_inputs[:,2], lw=1, c=c_ob)
+        ax_x.plot(self.data.masked_time, self.data.mx, lw=1)
+        ax_y.plot(self.data.masked_time, self.data.my, lw=1)
 
-  
         ## Compute stuff
         ## -------------
         fm = median(self.data.masked_flux)
@@ -179,17 +168,16 @@ class Detrender(object):
         ## ------
         ax_a.plot(self.data.masked_time, self.data.masked_flux/fm, c='0.75', lw=1)
         ax_a.plot(self.tr_data.unmasked_time, self.tr_data.unmasked_flux/fm, '.k', ms=6)
-        ax_a.plot(*self.data.outliers, c=c_bo, ls='', marker='o', ms=6)
-        ax_a.plot(self.data.unmasked_time[ms], (fd[ms] - 0.7*fptp)/fm, lw=1, c=c_ob)
+        ax_a.plot(*self.data.outliers, ls='', marker='o', ms=6)
+        ax_a.plot(self.data.unmasked_time[ms], (fd[ms] - 0.7*fptp)/fm, lw=1)
 
-        ax_a.plot(self.time, (mp-1.4*fptp)/fm, c=c_bo, lw=1)
+        ax_a.plot(self.time, (mp-1.4*fptp)/fm, lw=1)
 
         samples = permutation(self.time.size)[:maxpt]
 
-        ax_c.tripcolor(self.data.masked_inputs[samples,1], self.data.masked_inputs[samples,2], mp[samples])
-        ax_c.plot(self.tr_data.masked_inputs[:,1], self.tr_data.masked_inputs[:,2],'.', ms=3, c='w', alpha=0.8)
-        ax_c.plot(self.tr_data.masked_inputs[:,1], self.tr_data.masked_inputs[:,2],'.', ms=1.5, c='k')
-
+        ax_c.tripcolor(self.data.mx[samples], self.data.my[samples], mp[samples])
+        ax_c.plot(self.tr_data.mx, self.tr_data.my, '.', ms=3, c='w', alpha=0.8)
+        ax_c.plot(self.tr_data.mx, self.tr_data.my, '.', ms=1.5, c='k')
 
         setp(ax_a, ylim=(0.999*(fmin-1.4*fptp)/fm, 1.001*fmax/fm))
         setp(ax_a.get_xticklabels()+ax_y.get_xticklabels(), visible=False)
@@ -201,82 +189,3 @@ class Detrender(object):
 
         if fname:
             fig.savefig(fname)
-
-
-
-class DtData(object):
-    """Utility class that encapsulates the fluxes and inputs for a detrender.
-    """
-    def __init__(self, flux, inputs, mask=None):
-        self.flux  = array(flux)
-        self.inputs = array(inputs)
-        self.quality_mask = array(mask) if mask is not None else ones(self.flux.size, np.bool) 
-        self.outlier_mask = ones(self.flux.size, np.bool)
-        self.mask  =  self.quality_mask & self.outlier_mask
-        self.npt = self.flux.size
-        self._fm, self._fs =  medsig(self.flux[self.mask])
-
-        assert self.flux.ndim == 1, 'The flux array for DtData should be 1D [npt]'
-        assert self.inputs.ndim == 2, 'The input array for DtData should be 2D [npt,3]'
-        assert self.inputs.shape[1] == 3, 'The input array for DtData should be 2D with the shape [npt,3]'
-        assert self.quality_mask.ndim == 1, 'The mask array for DtData should be 1D [npt]'
-        ##TODO: Change the input array axes from [npt,3] to [3,npt]
-
-        
-    def create_training_set(self, nrandom=100, nblocks=5, bspan=50):
-        """Creates a new DtData instance containing a subset of the original data.
-        """
-        ids = (unique((permutation(self.npt-bspan)[:nblocks].reshape([nblocks,-1]) + arange(bspan)).ravel()))
-        ids = np.sort(concatenate([ids, permutation(delete(arange(self.npt),ids))[:nrandom]]))
-        tr_mask = zeros(self.npt, np.bool)
-        tr_mask[ids] = 1
-        tr_mask = (tr_mask & self.mask)
-        trd = DtData(self.unmasked_flux[tr_mask], self.unmasked_inputs[tr_mask])
-        trd._fm, trd._fs = self._fm, self._fs
-        return trd
-
-
-    def set_mask(self, outlier_mask):
-        self.outlier_mask = outlier_mask
-        self.mask = self.quality_mask & self.outlier_mask
-
-    @property
-    def masked_time(self):
-        return self.time[self.mask]
-
-    @property
-    def unmasked_time(self):
-        return self.time
-
-    @property
-    def masked_flux(self):
-        return self.flux[self.mask]
-
-    @property
-    def unmasked_flux(self):
-        return self.flux
-
-    @property
-    def masked_normalised_flux(self):
-        return self.flux[self.mask] / self._fm - 1.
-
-    @property
-    def unmasked_normalised_flux(self):
-        return self.flux / self._fm - 1.
-
-    @property
-    def masked_inputs(self):
-        return self.inputs[self.mask,:]
-
-    @property
-    def unmasked_inputs(self):
-        return self.inputs
-
-    @property
-    def outliers(self):
-        return self.inputs[~self.outlier_mask,0], self.flux[~self.outlier_mask]
-
-    @property
-    def time(self):
-        return self.inputs[:,0]
-
---
+++ src/dtdata.py
@@ -0,0 +1,160 @@
+from __future__ import division
+
+import math as m
+import numpy as np
+
+from numpy import nan, any, array, asarray, ones, arange, delete, where
+from numpy.random import permutation
+from utils import medsig
+from core import *
+
+class DtData(object):
+    """Utility class that encapsulates the fluxes and inputs for a detrender.
+    """
+    def __init__(self, flux, inputs, mask=None):
+        self._flux   = array(flux)
+        self._inputs = array(inputs)
+        self._mask   = array(mask) if mask is not None else ones(self._flux.size, np.bool) 
+        self._fm, self._fs = medsig(self.masked_flux)
+        self.nptm     = self.masked_flux.size
+        self.nptu     = self.unmasked_flux.size
+
+        assert self._flux.ndim == 1, 'The flux array for DtData should be 1D [npt]'
+        assert self._inputs.ndim == 2, 'The input array for DtData should be 2D [npt,3]'
+        assert self._inputs.shape[1] == 3, 'The input array for DtData should be 2D with the shape [npt,3]'
+
+        self.mf, self.uf = self.masked_flux,   self.unmasked_flux
+        self.mi, self.ui = self.masked_inputs, self.unmasked_inputs
+        self.mt, self.ut = self.masked_time,   self.unmasked_time
+        self.mx, self.ux = self.masked_x,      self.unmasked_x
+        self.my, self.uy = self.masked_y,      self.unmasked_y
+
+        
+    def create_training_set(self, nrandom=100, nblocks=5, bspan=50, max_tries=100):
+        """Creates a new DtData instance containing a subset of the original data.
+
+        Creates a new DtData instance made of nblocks non-overlapping chunks of bspan
+        consecutive datapoints (excluding the masked points) and nrandom datapoints
+        drawn randomly from the leftover points. This is done in order to try to include
+        both short- and long-time-scale trends to the training set.
+
+        Parameters
+        ----------
+        nrandom : int, optional
+                  Number of randomly drawn points
+
+        nblocks : int, optional
+                  Number of blocks to draw
+
+        bspan   : int, optional
+                  Size of a block
+
+        max_tries : int, optional
+                    Maximum number of tries to create a consecutive block that doesn't
+                    overlap with any of the previously created blocks
+
+        Returns
+        -------
+        DtData
+            The training dataset as a new DtData instance.
+        
+        """
+        ids = where(self.mask)[0]                    ## Get the masked indices
+        bstarts = np.full(nblocks, -bspan, np.int)   ## Starting indices for blocks
+
+        ## Chunks
+        ## ------
+        ## Attempt to draw nblock non-overlapping chunks of bspan consecutive points
+        ## from the data excluding the masked points. If this fails, the leftover
+        ## points will be added to the random sample set.
+        i, j = 0, 0
+        while True:
+            if j == nblocks or i > max_tries:
+                break
+
+            idx = np.random.randint(0, ids.size-bspan)
+            if not any((idx >= bstarts) & (idx < bstarts+bspan)):
+                bstarts[j] = idx
+                j += 1
+            i += 1
+    
+        block_ids = (np.sort(bstarts)[:,np.newaxis] + arange(bspan)[np.newaxis,:]).ravel()
+        npt_blocks = block_ids.size
+        npt_random = min(ids.size, nrandom+bspan*nblocks-npt_blocks)
+
+        idb = ids[block_ids]
+        idr = np.sort((permutation(delete(ids, block_ids))[:npt_random]))
+
+        tr_ids = np.sort(np.r_[idr, idb])
+        trd = DtData(self.unmasked_flux[tr_ids], self.unmasked_inputs[tr_ids])
+        trd._fm, trd._fs = self._fm, self._fs
+        return trd
+
+
+    @property
+    def flux_median(self):
+        return self._fm
+        
+    @property
+    def flux_std(self):
+        return self._fs
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @mask.setter
+    def mask(self, mask):
+        mask = array(mask)
+        assert mask.ndim == 1, 'The mask array for DtData should be 1D'
+        assert mask.size == self.flux.size,  'The mask array for DtData should have the same size as the flux array'
+        assert mask.dtype == np.bool,  'The mask array should be boolean'
+        self._mask = mask
+
+    @property
+    def masked_time(self):
+        return self._inputs[self.mask,0]
+
+    @property
+    def unmasked_time(self):
+        return self._inputs[:,0]
+
+    @property
+    def masked_flux(self):
+        return self._flux[self.mask]
+
+    @property
+    def unmasked_flux(self):
+        return self._flux
+
+    @property
+    def masked_normalised_flux(self):
+        return self._flux[self.mask] / self._fm - 1.
+
+    @property
+    def unmasked_normalised_flux(self):
+        return self._flux / self._fm - 1.
+
+    @property
+    def masked_inputs(self):
+        return self._inputs[self.mask, :]
+
+    @property
+    def unmasked_inputs(self):
+        return self._inputs
+
+    @property
+    def masked_x(self):
+        return self._inputs[self.mask, 1]
+
+    @property
+    def unmasked_x(self):
+        return self._inputs[:, 1]
+
+    @property
+    def masked_y(self):
+        return self._inputs[self.mask, 2]
+
+    @property
+    def unmasked_y(self):
+        return self._inputs[:, 2]
